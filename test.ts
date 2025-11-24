687461201fff8b92fa196bdaf97197f9ce817dd7
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 18:49:22 +0000

    test: convert 2

--- test.ts
+++ test.ts
@@ -6,4 +6,7 @@ describe("Basic tests", () => {
   it("Should output '1' when input is 1", () => {
     assert.equal(Convert(1), '1' );
   })
+  it("Should output '2' when input is 2", () => {
+    assert.equal(Convert(2), '2' );
+  })
 })
