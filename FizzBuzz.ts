a3b5dbdb94d57a3854abdb85106ae9c2f11cf5d3
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 19:48:04 +0000

    feat: convert 5

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,5 +1,6 @@
 const Convert = (number: number) => {
   if (Number.isInteger(number / 3)) return "Fizz"; // more readable than using % modulus
+  if (number === 5) return "Buzz";
 
   return number.toString();
 };
