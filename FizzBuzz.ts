bff646fb5fd10a3aa604987f1cf22fa174fe5c68
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 20:17:02 +0000

    feat: convert 30

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -6,6 +6,8 @@ const Convert = (number: number) => {
    * (which is older and was available from July 2015 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder#:~:text=In%20JavaScript%2C%20the%20modulo%20operation,the%20divisor%20y%20is%200n%20 )
    */
   if (number === 15) return "FizzBuzz";
+  if (number === 30) return "FizzBuzz";
+
   if (Number.isInteger(number / 3)) return "Fizz";
   if (Number.isInteger(number / 5)) return "Buzz";
 
