41f80d1c535866f7f4596f63a28db9f1de85e1f7
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 19:08:59 +0000

    refactor: use strict equal for best practice

--- test.ts
+++ test.ts
@@ -4,13 +4,13 @@ import Convert from './fizzbuzz.ts';
 
 describe("Basic tests", () => {
   it("Should output '1' when input is 1", () => {
-    assert.equal(Convert(1), '1' );
+    assert.strictEqual(Convert(1), '1' );
   })
   it("Should output '2' when input is 2", () => {
-    assert.equal(Convert(2), '2' );
+    assert.strictEqual(Convert(2), '2' );
   })
   it("Should output '4' when input is 4", () => {
-    assert.equal(Convert(4), '4' );
+    assert.strictEqual(Convert(4), '4' );
   })
 })
 
