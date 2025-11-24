8b8cffad3ca385b0fc1c5fe505747d31f36ed9c3
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 20:11:43 +0000

    refactor: "Buzz" feature

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,8 +1,12 @@
 const Convert = (number: number) => {
-  if (Number.isInteger(number / 3)) return "Fizz"; // more readable than using % modulus
-  if (number === 5) return "Buzz";
-  if (number === 10) return "Buzz";
-  if (number === 20) return "Buzz";
+  /*
+   * Using Number.isInteger
+   * (released September 2015 https://262.ecma-international.org/6.0/index.html#sec-arrow-function-definitions )
+   * Because it is more readable than using the % remainder operator
+   * (which is older and was available from July 2015 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder#:~:text=In%20JavaScript%2C%20the%20modulo%20operation,the%20divisor%20y%20is%200n%20 )
+   */
+  if (Number.isInteger(number / 3)) return "Fizz";
+  if (Number.isInteger(number / 5)) return "Buzz";
 
   return number.toString();
 };
