603ae297f5b07405436304a02d10df21e117ccb3
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-11-24 19:10:27 +0000

    feat: convert 3 to Fizz

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,5 +1,8 @@
 const Convert = (number: number) => {
   if(!Number.isSafeInteger(number)) throw new Error(`Number is not a Safe Integer: ${number}`);
+
+  if(number === 3) return 'Fizz';
+  
   return number.toString();
 }
 
